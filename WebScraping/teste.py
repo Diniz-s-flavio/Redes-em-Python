4deafebd97877bb31ade6ecb07fb56adbd7cb4f2
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2025-06-12 10:41:01 -0300

    shyuiaywgdb

--- WebScraping/teste.py
+++ WebScraping/teste.py
@@ -1,26 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url = "https://www.ifgoiano.edu.br/home/index.php/urutai.html"
-response = requests.get(url)
+dados = []
 
-if response.status_code == 200:
-    print("Page fetched successfuly")
-else:
-    print("Error fdd")
+url_https = "https://www.alura.com.br/artigos/conhecendo-o-modelo-osi?srsltid=AfmBOopOJElOXWM5m2Q9kmpHSpRxzZXJPTxjXYVgKOj4eZ-f_mYOOeUy"
+res_https = requests.get(url_https)
+soup_https = BeautifulSoup(res_https.content, "html.parser")
 
-soup = BeautifulSoup(response.content, "html.parser")
-# print(soup)
 
-# x = soup.find(id = "")
-# y = soup.find_all(class_ = "")
-# z = soup.find_all("")
+for tag in soup_https.find_all(["h1", "h2", "p"]):
+    texto = tag.get_text(strip=True)
+    if texto:
+        dados.append({"site": "Alura (HTTPS)", "conteudo": texto})
 
-title = soup.find("h1").text
-print(title)
+df = pd.DataFrame(dados)
+print(df)
 
-description = soup.find("p", class_="description").text
-print(description)
+url_http = "http://httpforever.com/"
+res_http = requests.get(url_http)
+soup_http = BeautifulSoup(res_http.content, "html.parser")
 
-description = soup.find_all("p", class_="description")
-print(description)
+for tag in soup_http.find_all(["h2", "p"]):
+    texto = tag.get_text(strip=True)
+    if texto:
+        dados.append({"site": "HTTPForever (HTTP)", "conteudo": texto})
+
+df = pd.DataFrame(dados)
+print(df)
+
+# df.to_csv("topicos_https_http.csv", index=False)
